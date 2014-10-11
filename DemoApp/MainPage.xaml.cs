719b8254f5743a827a985c99a9e95822a6b50a1b
neomahesh <maheshdahal@live.com>
2014-10-11 08:43:20 +0545

    Final

---
+++ DemoApp/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using DemoApp.DataReference;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DemoApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// 
+    /// 
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            call();
+
+            
+        }
+
+        public async void  call(){
+        
+         try
+            {
+
+                 DataModelSoapClient proxy = new DataModelSoapClient();
+                var result = await proxy.GetNewsAsync();
+
+                show s = new show();
+
+              lblNewsHeadLine.Text = result.Body.GetNewsResult.NewsHead.ToString();
+              //= result[4].Photo.ToString();
+              
+             
+            lblNewsDetails.Text = result.Body.GetNewsResult.NewsDetail.ToString();
+           //     s.Adv2 = result[4].Adv1.ToString();
+           //     s.NewsDescription = result[4].NewsDescription.ToString();
+
+
+              
+            
+            }
+            catch (Exception ex)
+            {
+                string s = ex.ToString();
+            }
+        
+        }
+
+      
+        }
+
+
+      
+    }
